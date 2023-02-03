1648fe8742b99a0faf47091d6a6e60cb50ad2570
JAntonioVR <juanantoniovr98@gmail.com>
2023-02-03 17:31:03 +0100

    Creadas actividades de prueba (#1)

---
+++ js/model.js
@@ -0,0 +1,44 @@
+
+var actividades = [
+    {
+        nombre: "Actividad 1",
+        fecha: new Date("2022-06-12"),
+        descripcion: `
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make 
+            a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
+            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
+            Lorem Ipsum passages, and more recently with desktop publishing software like 
+            Aldus PageMaker including versions of Lorem Ipsum.
+        `,
+        //images : []
+    },
+
+    {
+        nombre: "Actividad 2",
+        fecha: new Date(["2021-06-12", "9:30"]),
+        descripcion: `
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking 
+            at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, 
+            as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing 
+            packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' 
+            will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes 
+            by accident, sometimes on purpose (injected humour and the like).
+        `,
+        //images : []
+    },
+
+    {
+        nombre: "Actividad 3",
+        fecha: new Date(["2022-04-10", "17:30"]),
+        descripcion: `
+            There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in 
+            some form, by injected humour, or randomised words which don't look even slightly believable. If you are going 
+            to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. 
+        `,
+        //images : []
+    },
+
+]
+
+export { actividades }
